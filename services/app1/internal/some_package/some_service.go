06643d36268fe8b48000aa9b366df9dd1856defc
Max Berndt <mb@maxberndt.com>
2024-04-09 20:37:41 +0200

    do more with the set

--- services/app1/internal/some_package/some_service.go
+++ services/app1/internal/some_package/some_service.go
@@ -1,7 +1,10 @@
 package some_package
 
+import "fmt"
+
 type stringSet interface {
 	Add(key string)
+	Contains(key string) bool
 }
 
 type someService struct {
@@ -13,5 +16,8 @@ func New(stringSet stringSet) *someService {
 }
 
 func (s *someService) Work() {
-	s.stringSet.Add("testing")
+	key := "testing"
+	fmt.Printf("adding %s to my set...\n", key)
+	s.stringSet.Add(key)
+	fmt.Printf("checking if %s is contained in the set: %t\n", key, s.stringSet.Contains(key))
 }
